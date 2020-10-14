f56ffe45d66c2c6448d71a25b1e7d82000c77be1
imhilla <hillaryodhiambo282@gmail.com>
2020-10-14 07:05:25 +0300

    add hello world

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <div className='hello'>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <HelloWorld />,
   document.getElementById('root')
 );
 
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
