64594f4153a7f45966b2058240ee961498ebfcf8
imhilla <hillaryodhiambo282@gmail.com>
2020-10-14 18:37:36 +0300

    generate keys

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Button from './Button'
+import Button from './Button';
 
 const ButtonPanel = () => {
-  let array = {
+  const array = {
     group1: ['AC', '+/-', '%'],
     group2: ['7', '8', '9'],
     group3: ['4', '5', '6'],
@@ -10,27 +10,31 @@ const ButtonPanel = () => {
     group5: ['.'],
   };
 
+  const generateKey = (pre) => {
+    return `${pre}_${new Date().getTime()}`;
+  }
+
   return (
     <div className="button-container">
       <div className="button-container-div">
-        {array.group1.map((item, i) => <Button name={item} key={i + 1} />)}
+        {array.group1.map((item, i) => <Button name={item} key={generateKey(item)} />)}
         <Button name="÷" color="orange" />
       </div>
       <div className="button-container-div">
-        {array.group2.map((item, i) => <Button name={item} key={i + 1} />)}
+        {array.group2.map((item, i) => <Button name={item} key={generateKey(item)} />)}
         <Button name="X" color="orange" />
       </div>
       <div className="button-container-div">
-        {array.group3.map((item, i) => <Button name={item} key={i + 1} />)}
+        {array.group3.map((item, i) => <Button name={item} key={generateKey(item)} />)}
         <Button name="-" color="orange" />
       </div>
       <div className="button-container-div">
-        {array.group4.map((item, i) => <Button name={item} key={i + 1} />)}
+        {array.group4.map((item, i) => <Button name={item} key={generateKey(item)} />)}
         <Button name="+" color="orange" />
       </div>
       <div className="button-container-div">
         <Button name="0" wide={true} />
-        {array.group5.map((item, i) => <Button name={item} key={i + 1} />)}
+        {array.group5.map((item, i) => <Button name={item} key={generateKey(item)} />)}
         <Button name="=" color="orange" />
       </div>
     </div>
