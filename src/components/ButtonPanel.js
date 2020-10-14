265216fe335744a57b28eb2d0320488fabd9ba2f
imhilla <hillaryodhiambo282@gmail.com>
2020-10-14 13:49:36 +0300

    add button type

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,38 +4,35 @@ import PropTypes from 'prop-types';
 const ButtonPanel = () => (
   <div>
     <div>
-      <button type='button'>AC</button>
-      <button type='button'>+/-</button>
-      <button type='button'>%</button>
-      <button type='button'>÷</button>
+      <button type="button">AC</button>
+      <button type="button">+/-</button>
+      <button type="button">%</button>
+      <button type="button">÷</button>
     </div>
     <div>
-      <button type='button'>7</button>
-      <button type='button'>8</button>
-      <button type='button'>9</button>
-      <button type='button'>X</button>
+      <button type="button">7</button>
+      <button type="button">8</button>
+      <button type="button">9</button>
+      <button type="button">X</button>
     </div>
     <div>
-      <button type='button'>4</button>
-      <button type='button'>5</button>
-      <button type='button'>6</button>
-      <button type='button'>-</button>
+      <button type="button">4</button>
+      <button type="button">5</button>
+      <button type="button">6</button>
+      <button type="button">-</button>
     </div>
     <div>
-      <button type='button'>1</button>
-      <button type='button'>2</button>
-      <button type='button'>3</button>
-      <button type='button'>+</button>
+      <button type="button">1</button>
+      <button type="button">2</button>
+      <button type="button">3</button>
+      <button type="button">+</button>
     </div>
     <div>
-      <button type='button'>0</button>
-      <button type='button'>.</button>
-      <button type='button'>=</button>
+      <button type="button">0</button>
+      <button type="button">.</button>
+      <button type="button">=</button>
     </div>
-  </div>)
+  </div>
+);
 
-ButtonPanel.propTypes = {
-  name: PropTypes.string
-}
-
-export default ButtonPanel
+export default ButtonPanel;
