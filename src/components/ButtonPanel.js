51ee5f276dd3ec7b03c374c472d33974a2da804f
imhilla <hillaryodhiambo282@gmail.com>
2020-10-14 18:21:28 +0300

    use single quotes

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -3,48 +3,38 @@ import Button from './Button'
 
 const ButtonPanel = () => {
   let array = {
-    group1: ["AC", "+/-", "%"],
-    group2: ["7", "8", "9"],
-    group3: ["4", "5", "6"],
-    group4: ["1", "2", "3"],
-    group5: ["."],
+    group1: ['AC', '+/-', '%'],
+    group2: ['7', '8', '9'],
+    group3: ['4', '5', '6'],
+    group4: ['1', '2', '3'],
+    group5: ['.'],
   };
 
   return (
     <div className="button-container">
       <div className="button-container-div">
-        {array.group1.map((item, i) => {
-          return <Button name={item} key={i} />
-        })}
+        {array.group1.map((item, i) => <Button name={item} />)}
         <Button name="÷" color="orange" />
       </div>
       <div className="button-container-div">
-        {array.group2.map((item, i) => {
-          return <Button name={item} key={i} />
-        })}
+        {array.group2.map((item, i) => <Button name={item} />)}
         <Button name="X" color="orange" />
       </div>
       <div className="button-container-div">
-        {array.group3.map((item, i) => {
-          return <Button name={item} key={i} />
-        })}
+        {array.group3.map((item, i) => <Button name={item} />)}
         <Button name="-" color="orange" />
       </div>
       <div className="button-container-div">
-        {array.group4.map((item, i) => {
-          return <Button name={item} key={i} />
-        })}
+        {array.group4.map((item, i) => <Button name={item} />)}
         <Button name="+" color="orange" />
       </div>
       <div className="button-container-div">
         <Button name="0" wide={true} />
-        {array.group5.map((item, i) => {
-          return <Button name={item} key={i} />
-        })}
+        {array.group5.map((item, i) => <Button name={item} />)}
         <Button name="=" color="orange" />
       </div>
     </div>
-  )
+  );
 };
 
 export default ButtonPanel;
