cf0a1d436ed622ce6bdeaefb8bd0a9bcdde0c14f
imhilla <hillaryodhiambo282@gmail.com>
2020-10-14 08:18:44 +0300

    use pure functions

--- src/index.js
+++ src/index.js
@@ -3,19 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-class HelloWorld extends React.Component {
-  render() {
-    return (
-      <div className='hello'>
-        <h1>Hello World</h1>
-      </div>
-    )
-  }
+export const HelloWorld = () => {
+  return (
+    <div className="hello">
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 ReactDOM.render(
   <HelloWorld />,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 serviceWorker.unregister();
