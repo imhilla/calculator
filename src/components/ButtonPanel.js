f3d2d08ab13cca6c68420e4f96d98d2d54643b64
imhilla <hillaryodhiambo282@gmail.com>
2020-10-14 18:03:49 +0300

    map through buttons

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,38 +1,50 @@
 import React from 'react';
 import Button from './Button'
 
-const ButtonPanel = () => (
-  <div className="button-container">
-    <div className="button-container-div">
-      <Button name="AC" />
-      <Button name="+/-" />
-      <Button name="%" />
-      <Button name="÷" color="orange" />
+const ButtonPanel = () => {
+  let array = {
+    group1: ["AC", "+/-", "%"],
+    group2: ["7", "8", "9"],
+    group3: ["4", "5", "6"],
+    group4: ["1", "2", "3"],
+    group5: ["."],
+  };
+
+  return (
+    <div className="button-container">
+      <div className="button-container-div">
+        {array.group1.map((item, i) => {
+          return <Button name={item} key={i} />
+        })}
+        <Button name="÷" color="orange" />
+      </div>
+      <div className="button-container-div">
+        {array.group2.map((item, i) => {
+          return <Button name={item} key={i} />
+        })}
+        <Button name="X" color="orange" />
+      </div>
+      <div className="button-container-div">
+        {array.group3.map((item, i) => {
+          return <Button name={item} key={i} />
+        })}
+        <Button name="-" color="orange" />
+      </div>
+      <div className="button-container-div">
+        {array.group4.map((item, i) => {
+          return <Button name={item} key={i} />
+        })}
+        <Button name="+" color="orange" />
+      </div>
+      <div className="button-container-div">
+        <Button name="0" wide={true} />
+        {array.group5.map((item, i) => {
+          return <Button name={item} key={i} />
+        })}
+        <Button name="=" color="orange" />
+      </div>
     </div>
-    <div className="button-container-div">
-      <Button name="7" />
-      <Button name="8" />
-      <Button name="9" />
-      <Button name="X" color="orange" />
-    </div>
-    <div className="button-container-div">
-      <Button name="4" />
-      <Button name="5" />
-      <Button name="6" />
-      <Button name="-" color="orange" />
-    </div>
-    <div className="button-container-div">
-      <Button name="1" />
-      <Button name="2" />
-      <Button name="3" />
-      <Button name="+" color="orange" />
-    </div>
-    <div className="button-container-div">
-      <Button name="0" wide={true} />
-      <Button name="." />
-      <Button name="=" color="orange" />
-    </div>
-  </div>
-);
+  )
+};
 
 export default ButtonPanel;
