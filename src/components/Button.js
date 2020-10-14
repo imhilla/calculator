ff95c2c2bb9b87a2167f0e264cc2f50821060db4
imhilla <hillaryodhiambo282@gmail.com>
2020-10-14 12:32:42 +0300

    prefer dstructuring

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = props => <div><h1>{props.name}</h1></div>;
+const { name } = this.props
+const Button = () => <div><h1>{name}</h1></div>;
 
 Button.propTypes = {
   name: PropTypes.string,
