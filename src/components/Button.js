dbde3fd4daa2ff9d1857b8ac88ed6ce2d9888aca
imhilla <hillaryodhiambo282@gmail.com>
2020-10-14 12:11:21 +0300

    fix button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = (props) => (
-  <div>
-    <h1>{props.name}</h1>
-  </div>
-);
+const Button = (props) => <div><h1>{props.name}</h1></div>;
 
 Button.propTypes = {
   name: PropTypes.string,
-}
+};
 
 Button.defaultProps = {
   name: 'claculator',
-}
+};
 
 export default Button;
