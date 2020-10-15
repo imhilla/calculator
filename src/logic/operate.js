78b094bd39adce5b6a73cb3bf063d842af3e9581
imhilla <hillaryodhiambo282@gmail.com>
2020-10-15 13:01:31 +0300

    fix lint errors

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,27 +1,26 @@
 import Big from 'big.js';
 
 const Operate = (() => {
-
   const operate = (numberOne, numberTwo, operation) => {
-    let total
-    let one;
-    let two;
-    one = Big(numberOne)
-    two = Big(numberTwo)
-    if (operation === "-") {
-      total = one - two
-    } else if (operation === "+") {
-      total = one + two
-    } else if (operation === "X") {
-      total = one * two
-    } else if (operation === "÷") {
-      total = one / two
-    } else if (operation === "%") {
-      total = (one / 100) * two
+    let total;
+    const one;
+    const two;
+    one = Big(numberOne);
+    two = Big(numberTwo);
+    if (operation === '-') {
+      total = one - two;
+    } else if (operation === '+') {
+      total = one + two;
+    } else if (operation === 'X') {
+      total = one * two;
+    } else if (operation === '÷') {
+      total = one / two;
+    } else if (operation === '%') {
+      total = (one / 100) * two;
     }
-    return total
-  }
-  return { operate }
+    return total;
+  };
+  return { operate };
 })();
 
-export default Operate
+export default Operate;
