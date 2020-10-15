4d9865f729cb3b1beccf1f9210aa772e76a962e9
imhilla <hillaryodhiambo282@gmail.com>
2020-10-15 11:52:36 +0300

    add final total

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -4,14 +4,15 @@ const Calculate = (() => {
   const calculate = (dataObject, buttonName) => {
     const { next, operation } = dataObject;
     let { total } = dataObject;
-    if (buttonName === operation) {
-      total = Operate.operate(total, next, buttonName);
-    }
-    dataObject.total = total;
 
-    return dataObject
-  }
-  return calculate
+    if (buttonName === operation) {
+      let finalTotal = Operate.operate(total, next, buttonName);
+      return finalTotal
+    }
+    dataObject = { total: finalTotal, next: next, operation: operation }
+    return dataObject;
+  };
+  return calculate;
 })();
 
 export default Calculate;
