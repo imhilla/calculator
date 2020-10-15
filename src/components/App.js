b49b7ebc39b4a4ff771cd2a6e7867c31ecce291f
imhilla <hillaryodhiambo282@gmail.com>
2020-10-15 13:41:05 +0300

    add state and bind it

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,27 @@ import ButtonPanel from './ButtonPanel';
 import './App.css';
 // eslint-disable-next-line
 import Calculate from '../logic/calculate';
+import { render } from '@testing-library/react';
 
-const App = () => (
-  <div className="main-container">
-    <Display />
-    <ButtonPanel />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { total: null, next: null, operation: null }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+
+  }
+  render() {
+
+    return (
+      <div className="main-container">
+        <Display />
+        <ButtonPanel />
+      </div>
+    )
+  }
+};
 
 export default App;
