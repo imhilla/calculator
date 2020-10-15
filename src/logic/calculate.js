60eba28d2709329828955be580fde347a5c29c8e
imhilla <hillaryodhiambo282@gmail.com>
2020-10-15 12:13:02 +0300

    fix parse error

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -3,13 +3,9 @@ import Operate from './operate';
 const Calculate = (() => {
   const calculate = (dataObject, buttonName) => {
     const { total, next, operation } = dataObject;
-    const finalTotal
     if (buttonName === operation) {
-      finalTotal = Operate.operate(total, next, buttonName);
-      return finalTotal
+      Operate.operate(total, next, buttonName);
     }
-    const newdataObject = { total: finalTotal, next, operation };
-    return newdataObject;
   };
   return calculate;
 })();
