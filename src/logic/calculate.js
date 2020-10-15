c00f7fac940d8bce8790817229e0aec249a7d37f
imhilla <hillaryodhiambo282@gmail.com>
2020-10-15 12:18:17 +0300

    add new object

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -2,9 +2,13 @@ import Operate from './operate';
 
 const Calculate = (() => {
   const calculate = (dataObject, buttonName) => {
+    const newObj
+    const newTotal
     const { total, next, operation } = dataObject;
     if (buttonName === operation) {
-      Operate.operate(total, next, buttonName);
+      newTotal = Operate.operate(total, next, buttonName);
+      newObj = { total: newTotal, next, operation };
+      return newObj
     }
   };
   return calculate;
