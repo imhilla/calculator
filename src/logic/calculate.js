f58ee0b26acaf42e97ff23763f91bf11ce61b31c
imhilla <hillaryodhiambo282@gmail.com>
2020-10-15 12:40:56 +0300

    replace const with var

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -2,14 +2,11 @@ import Operate from './operate';
 
 const Calculate = (() => {
   const calculate = (dataObject, buttonName) => {
-    const newObj
-    const { total, next, operation } = dataObject;
+    var { total, next, operation } = dataObject;
     if (buttonName === operation) {
       Operate.operate(total, next, operation);
     }
-
-    newObj = { total, next, operation };
-    return newObj
+    const newObj = { total, next, operation }
   };
   return calculate;
 })();
